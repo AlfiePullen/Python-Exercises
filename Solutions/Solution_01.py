42bed238db38bbf6d216927e966ecfd492da6d5d
AlfiePullen <alfiepullen10@gmail.com>
2026-06-10 16:45:33 +0100

    Update Solution_01.py

--- Solutions/Solution_01.py
+++ Solutions/Solution_01.py
@@ -1,1 +1,15 @@
+#Solution_01.py
+
+def ex_01(x,y):
+    if x*y <= 1000:
+        return x*y
+    else:
+        return x+y
+
+#Test (a)
+
+print(ex_01(20, 30))
+print(ex_01(45,57))
+print(ex_01(500, 2)) #Testing the extreme cases: if x*y = 1000, it returns the product. 
+print(ex_01(1001, 1)) #Similarly, if x*y > 1000, the function returns the sum.
 
