c71b827bcd4d85326cd863f3e3a06443aec22b0a
AlfiePullen <alfiepullen10@gmail.com>
2026-06-12 12:09:11 +0100

    Create Exercise_05.py

---
+++ Exercises/Exercise_05.py
@@ -0,0 +1,18 @@
+# Assign an initial value to variable a
+a = 10
+# Assign an initial value to variable b
+b = 20
+# Display the values before swapping
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swap the values of a and b
+# The value of b is assigned to a
+# The value of a is assigned to b
+a, b = b, a
+
+# Display the values after swapping
+print("After swapping:")
+print("a =", a)
+print("b =", b)
