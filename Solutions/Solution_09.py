22db62d60ce5a4b9dbebe3d82053369e1f13dada
AlfiePullen <alfiepullen10@gmail.com>
2026-06-12 14:26:35 +0100

    Update Solution_09.py

--- Solutions/Solution_09.py
+++ Solutions/Solution_09.py
@@ -1,1 +1,9 @@
+sentance = input("Write a sentance:")
 
+num_vowel = 0
+
+#Converts all capitals into lowercase using .lower
+for i in sentance.lower():
+    if i in "aeiou":
+        num_vowel += 1
+print("There are", num_vowel, "vowels in this sentance" )
