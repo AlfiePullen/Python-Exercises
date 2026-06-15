9f699c492238a81b212f3bee346a775676b8a76a
AlfiePullen <alfiepullen10@gmail.com>
2026-06-15 13:53:24 +0100

    Create Solution_15.py

---
+++ Solutions/Solution_15.py
@@ -0,0 +1,7 @@
+for i in range(1,6):
+    for j in range(i):
+        print(i, end=" ") # end=" " keeps it on the same line
+    # New line after each row
+    print("\n")
+        
+#NOTE: \n is the new line character
